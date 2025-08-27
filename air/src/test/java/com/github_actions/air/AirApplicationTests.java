c352a562416ac69c9b76c7b461c0bf847342696a
KarthikSuresh7 <skarthikpec@gmail.com>
2025-08-27 13:10:21 +0530

    first commit

---
+++ air/src/test/java/com/github_actions/air/AirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github_actions.air;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
